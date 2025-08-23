ac12fcf63e65824172de3d61cc266c38401ccaf6
Vagner SV <vsvasconcelos@gmail.com>
2025-08-22 23:41:30 -0300

    Acrescentado acelerômetro

---
+++ build/CMakeFiles/mpu6500.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpu6500.dir/src/mpu6500.c.o"
+  "CMakeFiles/mpu6500.dir/src/mpu6500.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpu6500.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
